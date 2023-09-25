2ec8531c5a6b00a9c4104f0b6a589af475d9c85a
pcmagas <pcmagas@disroot.org>
2023-09-25 23:07:40 +0300

    Select appropriate xdebug version depending the php version.

--- utils/setup_xdebug.sh
+++ utils/setup_xdebug.sh
@@ -7,7 +7,17 @@ if [ -z ${XDEBUG_VERSION} ] && [ "${XDEBUG_VERSION}" != "" ] && [ "${XDEBUG_VERS
     install-php-extensions xdebug-${XDEBUG_VERSION}
 else
     echo "Installing xdebug latest version"
-    install-php-extensions xdebug
+    PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
+    PHP_VERSION="$(echo "${PHP_VERSION}" | tr -d '[:space:]')"
+
+    case "${PHP_VERSION}" in 
+     "7.1")install-php-extensions xdebug-2.9.8;;
+     "7.0")install-php-extensions xdebug-2.7.2;;
+     "5.6"|"5.5")install-php-extensions xdebug-2.5.5;;
+     "5.4")install-php-extensions xdebug-2.4.1;;
+     "5.3")install-php-extensions xdebug-2.2.6;;
+     *) install-php-extensions xdebug;;
+    esac
 fi
 
 mkdir -p /var/log/xdebug
