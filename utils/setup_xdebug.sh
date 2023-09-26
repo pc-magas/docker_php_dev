a03a7ff73fdb76c162a0ddcfc06d2ec790e0f480
pcmagas <pcmagas@disroot.org>
2023-09-26 09:56:48 +0300

    Add licence.

--- utils/setup_xdebug.sh
+++ utils/setup_xdebug.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+#
+#  Xdebug setup Script
+#  Copyright (C) 2023  Dimitrios Desyllas
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
 XDEBUG_VERSION="$(echo "${XDEBUG_VERSION}" | tr -d '[:space:]')"
 
 if [ -z ${XDEBUG_VERSION} ] && [ "${XDEBUG_VERSION}" != "" ] && [ "${XDEBUG_VERSION}" != "latest" ] ; then
@@ -16,7 +35,7 @@ else
      "5.6"|"5.5")install-php-extensions xdebug-2.5.5;;
      "5.4")install-php-extensions xdebug-2.4.1;;
      "5.3")install-php-extensions xdebug-2.2.6;;
-     *) install-php-extensions xdebug;;
+#) install-php-extensions xdebug;;
     esac
 fi
 
